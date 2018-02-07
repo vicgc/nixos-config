0f71a8700db36faf13be016945be19cf509605be
Andrei Vladescu-Olt <andrei@avolt.net>
2018-02-07 20:58:17 +0100

    update

--- configuration.nix
+++ configuration.nix
@@ -6,7 +6,6 @@ let
 in {
   imports =
     [
-      ./dnsmasq.nix
       ./docker-gc.nix
       ./udiskie.nix
       ./hardware-configuration.nix
@@ -198,5 +197,17 @@ in {
     latitude = "48.85";
     longitude = "2.35";
     temperature.night = 2500;
+  };
+
+  services.printing = {
+    enable = true;
+    clientConf = ''
+      <Printer default>
+        UUID urn:uuid:3c151d9e-3d44-3a04-59f9-5cdfbb513438
+        Info DCPL2520DW
+        MakeModel everywhere
+        DeviceURI ipp://192.168.1.15/ipp/print
+      </Printer>
+    '';
   };
 }
