7354a63a8a7a92fe7b94d7182909ecc98cc331f8
Andrei Vladescu-Olt <andrei@avolt.net>
2018-04-27 23:37:33 +0200

    update

--- darwin-configuration.nix
+++ darwin-configuration.nix
@@ -1,8 +1,6 @@
 { config, pkgs, ... }:
 
 {
-  # List packages installed in system profile. To search by name, run:
-  # $ nix-env -qaP | grep wget
   environment.systemPackages = with pkgs; [
     nodejs
     fastlane
@@ -13,17 +11,11 @@
     "LC_ALL" = "en_us.UTF-8";
     "LANG" = "en_us.UTF-8";
   };
-  # Create /etc/bashrc that loads the nix-darwin environment.
-  programs.bash.enable = true;
-  # programs.zsh.enable = true;
-  # programs.fish.enable = true;
-  
-  # Used for backwards compatibility, please read the changelog before changing.
-  # $ darwin-rebuild changelog
+
+  programs.zsh.enable = true;
+
   system.stateVersion = 2;
   
-  # You should generally set this to the total number of logical cores in your system.
-  # $ sysctl -n hw.ncpu
   nix.maxJobs = 1;
   nix.buildCores = 1;
 }
